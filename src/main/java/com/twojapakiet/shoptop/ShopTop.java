7deb29611f2251c44297f7f501ca370dedf0814e
GenOpenDiesel <83091816+GenOpenDiesel@users.noreply.github.com>
2025-10-22 20:42:47 +0200

    Update ShopTop.java

--- src/main/java/com/twojapakiet/shoptop/ShopTop.java
+++ src/main/java/com/twojapakiet/shoptop/ShopTop.java
@@ -1,6 +1,6 @@
 package com.twojapakiet.shoptop;
 
-import com.twojapakiet.shoptop.commands.ReloadCommand; 
+import com.twojapakiet.shoptop.commands.ReloadCommand;
 import com.twojapakiet.shoptop.data.DataManager;
 import com.twojapakiet.shoptop.listeners.TransactionListener;
 import com.twojapakiet.shoptop.placeholders.ShopStatsExpansion;
@@ -9,27 +9,31 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ShopTop extends JavaPlugin {
 
-    private static ShopTop instance; 
+    private static ShopTop instance;
     private DataManager dataManager;
     private static final long SAVE_INTERVAL = 20L * 60 * 5; // 5 minut
 
+    // NOWE POLE: Przechowujemy instancję ekspansji
+    private ShopStatsExpansion shopStatsExpansion;
+
     @Override
     public void onEnable() {
-        instance = this; 
+        instance = this;
         this.dataManager = new DataManager(this);
         dataManager.loadData();
 
         getServer().getPluginManager().registerEvents(new TransactionListener(dataManager), this);
 
+        // ZMIANA: Sprawdzamy PAPI i zapisujemy instancję ekspansji
         if (getServer().getPluginManager().isPluginEnabled("PlaceholderAPI")) {
-            new ShopStatsExpansion(this).register();
+            this.shopStatsExpansion = new ShopStatsExpansion(this);
+            this.shopStatsExpansion.register();
             getLogger().info("PlaceholderAPI znalezione. Rejestrowanie ekspansji 'sklep_staty'...");
         } else {
             getLogger().warning("PlaceholderAPI nie znalezione! Placeholdery nie będą działać.");
         }
-        
-        // ZMIANA TUTAJ: Rejestrujemy komendę ręcznie w CommandMap
-        // Usuwamy: getCommand("shoptop").setExecutor(new ReloadCommand(this));
+
+        // Rejestracja komendy (bez zmian)
         getServer().getCommandMap().register("shoptop", new ReloadCommand(this));
 
         Bukkit.getScheduler().runTaskTimerAsynchronously(this, () -> {
@@ -42,6 +46,12 @@ public final class ShopTop extends JavaPlugin {
 
     @Override
     public void onDisable() {
+        // ZMIANA: Wyrejestrujemy ekspansję przy wyłączaniu
+        if (this.shopStatsExpansion != null) {
+            this.shopStatsExpansion.unregister();
+            getLogger().info("Wyrejestrowano ekspansję PlaceholderAPI.");
+        }
+
         if (dataManager != null) {
             dataManager.saveData();
             getLogger().info("Wszystkie statystyki graczy zostały zapisane przed wyłączeniem pluginu.");
@@ -49,14 +59,37 @@ public final class ShopTop extends JavaPlugin {
         getLogger().info("Plugin ShopTop został wyłączony!");
     }
 
+    // ZMIANA: Kompletna przebudowa logiki przeładowania
     public void reloadPlugin() {
         getLogger().info("Rozpoczynanie przeładowania pluginu ShopTop...");
+
+        // 1. Zapisz bieżące dane
         dataManager.saveData();
+
+        // 2. Wyczyść stare dane z pamięci
         dataManager.clearData();
+
+        // 3. Załaduj dane na nowo z pliku
         dataManager.loadData();
+
+        // 4. Przeładuj PlaceholderAPI
+        if (getServer().getPluginManager().isPluginEnabled("PlaceholderAPI")) {
+            // Wyrejestruj starą instancję, jeśli istnieje
+            if (this.shopStatsExpansion != null) {
+                this.shopStatsExpansion.unregister();
+            }
+            // Stwórz i zarejestruj nową instancję
+            this.shopStatsExpansion = new ShopStatsExpansion(this);
+            this.shopStatsExpansion.register();
+            getLogger().info("Ekspansja PlaceholderAPI została pomyślnie przeładowana.");
+        } else {
+            getLogger().warning("PlaceholderAPI nie znalezione! Nie można przeładować ekspansji.");
+        }
+
         getLogger().info("Plugin ShopTop został pomyślnie przeładowany!");
     }
 
+    // Gettery (bez zmian)
     public static ShopTop getInstance() {
         return instance;
     }
